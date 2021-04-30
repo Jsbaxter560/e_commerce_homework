73e116ed70145657c31d849da0ec3d9d62e13319
John Baxter <juancho420@ymail.com>
2021-04-29 23:07:26 -0700

    some work on the routes... sadly, not nearly enough...

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -4,13 +4,20 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 router.use('./api/products', apiRoutes);
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  try {
+    const productData = await Product.findAll();
+    console.log(productData);
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  };
   // find all products
   // be sure to include its associated Category and Tag data
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => 
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
 });
